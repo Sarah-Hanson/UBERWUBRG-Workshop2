9b3ad23dabe9a0a08b44b7df1de1967084bce935
Sarah Hanson <57675216+Sarah-Hanson@users.noreply.github.com>
2020-01-23 13:11:18 -0700

    Additional framework
    should be able to get a decent idea of how my end of things is going to be called just by looking at this and extrapolate to other stuff

--- Workshop2-TravelExperts/Workshop2-TravelExperts/DBO.cs
+++ Workshop2-TravelExperts/Workshop2-TravelExperts/DBO.cs
@@ -6,17 +6,70 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Workshop2_TravelExperts {
+    /*
+     * Author: Sarah Hanson
+     * Object for handling the input and retreival of data from the DB to object classes to be used within the program
+     * Visible Functions: GetObjectListFromDB(5 Overloads), GetTableFromTB(5 Overloads)
+     */
     class DBO<T> {
-        public static BindingList<T> GetObjectListFromDB(string query) {
+        /*
+         * The get ObjectListFromDB function overlaod all serve to accept an object list from the caller and
+         * fill that list from the DB with the appropriate data from the table, they handle the queries and 
+         * data creation based on the inputed list type;
+         */
+        public static void GetObjectListFromDB(out BindingList<Product> products) {
+            products = new BindingList<Product>();
             using (SqlConnection dbConnect = TravelExpertsDB.GetConnection()) {
                 dbConnect.Open();
-
+                string query = "";
+                try {
+                    using (SqlCommand cmd = new SqlCommand(query, dbConnect)) {
+                        //run command and process results
+                        using (SqlDataReader reader = cmd.ExecuteReader(CommandBehavior.CloseConnection)) {
+                            while (reader.Read()) {
+                                Product o = new Product();
+                                { ReadFromDB(reader, "ProductID", out int output); o.ProductID = output; }
+                                { ReadFromDB(reader, "ProductName", out string output); o.ProductName = output; }
+                                products.Add(o);
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex) {
+                    MessageBox.Show(ex.Message);
+                }
                 dbConnect.Close();
-                return null;
             }
         }
+        public static void GetObjectListFromDB(out BindingList<Package> packages) {
+            packages = new BindingList<Package>();
+            using (SqlConnection dbConnect = TravelExpertsDB.GetConnection()) {
+                dbConnect.Open();
+                string query = "";
+                using (SqlCommand cmd = new SqlCommand(query, dbConnect)) {
+                    //run command and process results
+                    using (SqlDataReader reader = cmd.ExecuteReader(CommandBehavior.CloseConnection)) {
+                        while (reader.Read()) {
+                            Package o = new Package();
+                            { ReadFromDB(reader, "PackageID", out int output); o.PackageID = output; }
+                            { ReadFromDB(reader, "PackageName", out string output); o.PackageName = output; }
+                            { ReadFromDB(reader, "PackageName", out string output); o.PackageName = output; }
+                            packages.Add(o);
+                        }
+                        
+                    }
+                }
+                dbConnect.Close();
+            }
+        }
+        /*
+         * The get ObjectListFromDB function overlaod all serve to accept an object list from the caller and
+         * fill that list from the DB with the appropriate data from the table, they handle the queries and 
+         * data creation based on the inputed list type;
+         */
         public static BindingList<T> GetTableFromDB(string query) {
             using (SqlConnection dbConnect = TravelExpertsDB.GetConnection()) {
                 dbConnect.Open();
@@ -25,39 +78,52 @@ namespace Workshop2_TravelExperts {
                 return null;
             }
         }
-        static private string readFromDB(SqlDataReader r, string column, string val) {
+        /*
+         * The ReadFromDB functions all return a given column to the type given for the out value;
+         */
+        static private void ReadFromDB(SqlDataReader r, string column, out string val) {
             if (r[column] != DBNull.Value) {
-                return r[column].ToString();
+                val = r[column].ToString();
             }
             else {
-                return null;
+                val = null;
             }
         }
-        static private Nullable<DateTime> readFromDB(SqlDataReader r, String column, Nullable<DateTime> val) {
+        static private void ReadFromDB(SqlDataReader r, String column, out Nullable<DateTime> val) {
             if (r[column] != DBNull.Value) {
-                return Convert.ToDateTime(r[column].ToString());
+                val = Convert.ToDateTime(r[column].ToString());
             }
             else {
-                return null;
+                val = null;
             }
         }
-        static private Nullable<decimal> readFromDB(SqlDataReader r, String column, Nullable<decimal> val) {
+        static private void ReadFromDB(SqlDataReader r, String column, out Nullable<decimal> val) {
             if (r[column] != DBNull.Value) {
-                return Convert.ToDecimal(r[column].ToString());
+                val = Convert.ToDecimal(r[column].ToString());
             }
             else {
-                return null;
+                val = null;
             }
         }
-        static private Nullable<int> readFromDB(SqlDataReader r, String column, Nullable<int> val) {
+        static private void ReadFromDB(SqlDataReader r, String column, out Nullable<int> val) {
             if (r[column] != DBNull.Value) {
-                return Convert.ToInt32(r[column].ToString());
+                val = Convert.ToInt32(r[column].ToString());
             }
             else {
-                return null;
+                val = null;
             }
+        }
+        static private void ReadFromDB(SqlDataReader r, String column, out DateTime val) {
+            val = Convert.ToDateTime(r[column].ToString());
+        }
+        static private void ReadFromDB(SqlDataReader r, String column, out decimal val) {
+            val = Convert.ToDecimal(r[column].ToString());
+        }
+        static private void ReadFromDB(SqlDataReader r, String column, out int val) {
+            val = Convert.ToInt32(r[column].ToString());
         }
     }
+    // Connection string class for the TavelExpertsDB
     class TravelExpertsDB {
         public static SqlConnection GetConnection() {
             string connectionString =
