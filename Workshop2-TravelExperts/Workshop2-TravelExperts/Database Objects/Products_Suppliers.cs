7cc85448a9784b9a3fe61748d2b35d97cfa8cf11
Sarah <sarah.hanson@edu.sait.ca>
2020-02-14 14:15:31 -0700

    Revert "Revert "SARAH WHY""
    This reverts commit 6fb3e9057a2b5e3aba4aac4ebd2dc5ccacae278b.

--- Workshop2-TravelExperts/Workshop2-TravelExperts/Database Objects/Products_Suppliers.cs
+++ Workshop2-TravelExperts/Workshop2-TravelExperts/Database Objects/Products_Suppliers.cs
@@ -1,16 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 /*
  *  Author: Sarah Hanson
- *  Holds data for the Packages_Products_Suppliers table
+ *  Holds data for the Products_Suppliers table
  */
 namespace Workshop2_TravelExperts {
-    class Packages_Products_Suppliers {
-        public int PackageID { get; set; }
+    public class Products_Suppliers {
         public int ProductSupplierID { get; set; }
+        public int ProductID { get; set; }
     }
 }
