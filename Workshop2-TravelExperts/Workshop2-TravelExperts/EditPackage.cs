57f16efd6cd59d635525bc2efd78bcb5e29ea2df
826725 <826725@SAIT224886OOSD>
2020-02-14 12:12:14 -0700

    vv

--- Workshop2-TravelExperts/Workshop2-TravelExperts/EditPackage.cs
+++ Workshop2-TravelExperts/Workshop2-TravelExperts/EditPackage.cs
@@ -13,16 +13,41 @@ namespace Workshop2_TravelExperts
     public partial class EditPackage : Form
     {
         public Package package;
-        public EditPackage(Package pack) {
+        public EditPackage(Package pack)
+        {
             InitializeComponent();
             package = pack;
         }
-        private void EditPackage_Load(object sender, EventArgs e) {
+        private void EditPackage_Load(object sender, EventArgs e)
+        {
             this.DisplayPackage();
         }
-        private void btnSubmit_Click(object sender, EventArgs e) {
-
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            Package newPackage = new Package();
+            newPackage.PackageId = package.PackageId;
+            this.PutPackageData(newPack);
+            try
+            {
+                if (!TravelExpertsDB.UpdatePackage(package,newPack))
+                {
+                    MessageBox.Show("Another user has updated or " +
+                        "deleted that customer.", "Database Error");
+                    this.DialogResult = DialogResult.Retry;
+                }
+                else // success
+                {
+                    customer = newCustomer;
+                    this.DialogResult = DialogResult.OK;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, ex.GetType().ToString());
+            }
         }
+    
+
         private void DisplayPackage() {
             label1.Text = Convert.ToString(package.PackageId);
             txtPkgName.Text = package.PkgName;
@@ -35,6 +60,10 @@ namespace Workshop2_TravelExperts
 
         private void btnBack_Click(object sender, EventArgs e){ //Added by BC
             this.Close();
+        }
+        private void PutPackageData(Package package)
+        {
+
         }
     }
 }
